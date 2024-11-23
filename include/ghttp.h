07a9c9ef005e3b5fcf337edf75eaf08cf7a7b604
gimura2022 <gimura0001@gmail.com>
2024-11-23 21:24:43 +0300

    added new header and deleated code

--- include/ghttp.h
+++ include/ghttp.h
@@ -1,67 +1,92 @@
 #ifndef _http_server_h
 #define _http_server_h
 
-#include <stdint.h>
-#include <stddef.h>
-#include <stdbool.h>
-#include <sys/types.h>
+#include <cstddef>
+#include <stdlib.h>
 
-#define GHTTP_MAX_URL 1024
-#define GHTTP_MAX_HOST 1024
-#define GHTTP_MAX_RESPONCE_STR 1024
-#define GHTTP_MAX_SERVER 512
-#define GHTTP_MAX_CONTENT_TYPE 1024
-#define HTTP_BUFFER_SIZE 1024 * 64 
+#define GHTTP_MAX_NOT_PARSED_HEADERS 128
 
-enum ghttp__request_type {
-	HTTPRQT_GET = 0,
-	HTTPRQT_POST,
+struct ghttp__header {
+	const char* name;
+	const char* value;
+	void* value_converted;
 };
 
-struct ghttp__content {
-	char content_type[GHTTP_MAX_CONTENT_TYPE];
-	size_t content_size;
-	char content[HTTP_BUFFER_SIZE];
+struct ghttp__general_headers {
+	struct ghttp__header accept;
+	struct ghttp__header accept_ch;
+	struct ghttp__header accept_charset;
+	struct ghttp__header accept_encoding;
+	struct ghttp__header accept_language;
+	struct ghttp__header accept_ranges;
+	struct ghttp__header connection;	
+	struct ghttp__header content_encoding;
+	struct ghttp__header content_language;
+	struct ghttp__header content_lenght;
+	struct ghttp__header content_location;
+	struct ghttp__header content_range;
+	struct ghttp__header content_type;
+	struct ghttp__header date;
+	struct ghttp__header pragma;
+	struct ghttp__header transfer_encoding;
+	struct ghttp__header user_agent;
+	struct ghttp__header vary;
+	struct ghttp__header via;
+	struct ghttp__header warning;
 };
 
-struct ghttp__responce {
-	int responce_code;
-	char responce_str[GHTTP_MAX_RESPONCE_STR];
+struct ghttp__responce_headers {
+	struct ghttp__general_headers general_headers;
 
-	char server[GHTTP_MAX_SERVER];
+	struct ghttp__header age;
+	struct ghttp__header allow;
+	struct ghttp__header location;
+	struct ghttp__header proxy_authenticate;
+	struct ghttp__header range;
+	struct ghttp__header server;
+	struct ghttp__header www_authenticate;
+};
 
-	struct { struct ghttp__content d; bool h; } content;
+struct ghttp__request_headers {
+	struct ghttp__general_headers general_headers;
+
+	struct ghttp__header autorisation;
+	struct ghttp__header expect;
+	struct ghttp__header from;
+	struct ghttp__header host;
+	struct ghttp__header if_match;
+	struct ghttp__header if_range;
+	struct ghttp__header if_unmodified_since;
+	struct ghttp__header max_forwards;
+	struct ghttp__header proxy_authorization;
+	struct ghttp__header referer;
+	struct ghttp__header retry_after;
+	struct ghttp__header tf;
+	struct ghttp__header upgrade;
 };
 
 struct ghttp__request {
-	enum ghttp__request_type type;
-	char url[GHTTP_MAX_URL];
+	const char* method;
+	const char* url;
 
-	char host[GHTTP_MAX_HOST];
-
-	struct { struct ghttp__content d; bool h; } content;
+	struct ghttp__request_headers request_headers;
+	struct ghttp__header not_parssed_headers[GHTTP_MAX_NOT_PARSED_HEADERS];
+	size_t not_parssed_headers_count;
 };
 
-struct ghttp__server_context {
-	bool runned;	
+struct ghttp__responce {
+	int code;
+	const char* msg;
+
+	struct ghttp__responce_headers responce_headers;
+	struct ghttp__header not_parssed_headers[GHTTP_MAX_NOT_PARSED_HEADERS];
+	size_t not_parssed_headers_count;
 };
 
-typedef struct ghttp__responce (*ghttp__respoder_t)(struct ghttp__server_context*, struct ghttp__request);
+void ghttp__create_responce(char* buf, const size_t buf_size, const struct ghttp__responce* responce);
+void ghttp__create_request(char* buf, const size_t buf_size, const struct ghttp__request* request);
 
-struct ghttp__path_responder {
-	const char* path;
-	const ghttp__respoder_t responder;
-	const enum ghttp__request_type request_type;
-	const bool use_regex;
-};
-
-struct ghttp__server_data {
-	uint16_t port;	
-};
-
-struct ghttp__server_data ghttp__get_default_server_data(void);
-
-void ghttp__start_server(struct ghttp__server_data server_data, struct ghttp__path_responder* responders,
-		size_t responder_count, ghttp__respoder_t not_found_responder);
+bool ghttp__parse_responce(const char* buf, const size_t buf_size, struct ghttp__responce* responce);
+bool ghttp__parse_request(const char *buf, const size_t buf_size, struct ghttp__request* request);
 
 #endif
