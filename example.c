c043231ecc5ac206f8ca1e1d959b57ef0ed673de
gimura2022 <gimura0001@gmail.com>
2024-11-24 20:44:20 +0300

    added new example

--- example.c
+++ example.c
@@ -1,36 +1,49 @@
+#include <ghttp.h>
+#include <ghttp_msg.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "include/ghttp.h"
 
-static struct ghttp__responce handler(struct ghttp__server_context* ctx, struct ghttp__request request)
+static struct ghttp__responce handler(struct ghttp__server_context* ctx, struct ghttp__request* request)
 {
 	printf("Prinal\n");
 
 	struct ghttp__responce responce = {0};
 	responce.responce_code = 200;
+	responce.content = malloc(100);
+	memset(responce.content, '\0', 100);
+	strcpy(responce.content, "Hello, world!");
+
+	responce.responce_str = malloc(100);
 	strcpy(responce.responce_str, "OK");
 
-	responce.content.h = true;
-	strcpy(responce.content.d.content_type, "text/html");
+	responce.headers.server.value = malloc(100);
+	strcpy(responce.headers.server.value, "Gimura server");
 
-	strcpy(responce.content.d.content, "main(){puts(\"Hello, world!\");}");
-	responce.content.d.content_size = strlen(responce.content.d.content);
+	responce.headers.general.content_length.value = malloc(100);
+	strcpy(responce.headers.general.content_length.value, "100");
 
-	strcpy(responce.server, "Gimura Server");
+	responce.headers.general.content_type.value = malloc(100);
+	strcpy(responce.headers.general.content_type.value, "text/html");
 
 	return responce;
 }
 
 int main(int argc, char* argv[])
 {
-	ghttp__start_server(ghttp__get_default_server_data(), &(struct ghttp__path_responder) {
-				.path = "/",
-				.responder = handler,
-				.request_type = HTTPRQT_GET,
-				.use_regex = false
-			}, 1, NULL);	
+	ghttp__init(malloc, free);
+
+	ghttp__start_server((struct ghttp__server_data) {
+				.port = 8080
+			}, &(struct ghttp__path_responder) {
+				.path         = "/",
+				.responder    = handler,
+				.request_type = "GET",
+				.use_regex    = false,
+			}, 1, NULL);
 
 	return 0;
 }
